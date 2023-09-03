f664ef0b61c12bc13b126eb8d9b7166963031589
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-09-03 11:37:16 -0300

    locaweb

--- javascript/js/script1.js
+++ javascript/js/script1.js
@@ -1,17 +1,57 @@
 class Veiculo {
 
     constructor() {
-        this.id = 0;
-        this.nomeVeiculo = '';
-        this.valor = 0;
+        this.id = 1;
+        this.arrayVeiculos = [];
     }
 
-    adicionar() {
-        alert("Adicionar um novo veículo: ")
+    salvar() {
+        let veiculo = this.lerDados();
+
+        if(this.validarCampos(veiculo)) {
+            this.adicionar(veiculo);
+        }
+
+        console.log(this.arrayVeiculos);
     }
 
-    excluir() {
-        alert("Veículo excluído!")
+    adicionar(veiculo){
+        this.arrayVeiculos.push(veiculo);
+        this.id++;
+    }
+
+    lerDados() {
+        let veiculo = {}
+
+        veiculo.id = this.id;
+        veiculo.nomeVeiculo = document.getElementById('veiculo').value;
+        veiculo.preco = document.getElementById('preco').value;
+
+        return veiculo;
+    }
+
+    validarCampos(veiculo) {
+        let msg = '';
+
+        if(veiculo.nomeVeiculo == ''){
+            msg += '- Informe o nome do Veículo \n';
+        }
+
+        if(veiculo.preco == '') {
+            msg += '- Informe o Preço do Veículo \n';
+        }
+        
+        if(msg != '') {
+            alert(msg);
+            return false;
+        }
+
+        return true;
+
+    }
+
+    cancelar() {
+        
     }
 }
 
