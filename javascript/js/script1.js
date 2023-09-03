b4ebfd6ce97c93701d6d0c8e83b3872840c47826
Jose-Herculano-Neto <herculano.neto@hotmail.com>
2023-09-03 08:10:43 -0300

    local

---
+++ javascript/js/script1.js
@@ -0,0 +1,18 @@
+class Veiculo {
+
+    constructor() {
+        this.id = 0;
+        this.nomeVeiculo = '';
+        this.valor = 0;
+    }
+
+    adicionar() {
+        alert("Adicionar um novo veículo: ")
+    }
+
+    excluir() {
+        alert("Veículo excluído!")
+    }
+}
+
+var veiculo = new Veiculo()
